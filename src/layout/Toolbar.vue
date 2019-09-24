51bd3be19d0292adca71b3109761ca578d4f5ba9
Teun Huibregtse <huib0029@hz.nl>
2019-09-24 20:46:52 +0200

    Moved toolbar and changed text

---
+++ src/layout/Toolbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar app>
+    <v-toolbar-title class="headline text-uppercase">
+      <span>HZ HBO-ICT</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <span class="font-weight-light">
+      De grote HBO-ICT geschikt
+        <v-icon>mdi-checkbox-marked-outline</v-icon> of ongeschikt
+        <v-icon>mdi-checkbox-blank-outline</v-icon> tool
+        </span>
+    <v-spacer></v-spacer>
+    <v-btn class="ma-2" outlined color="success" href="https://hz.nl/opleidingen/hbo-ict" target="_blank">
+      <span>Schrijf je in!</span>
+    </v-btn>
+  </v-app-bar>
+</template>
