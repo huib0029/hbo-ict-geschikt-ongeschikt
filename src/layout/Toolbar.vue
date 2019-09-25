3468e18796e53c8306b1582418a76d2154408fc4
Teun Huibregtse <huib0029@hz.nl>
2019-09-25 10:41:40 +0200

    Added basic quiz layout

--- src/layout/Toolbar.vue
+++ src/layout/Toolbar.vue
@@ -4,14 +4,18 @@
       <span>HZ HBO-ICT</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <span class="font-weight-light">
+    <span text-no-wrap class="font-weight-light">
       De grote HBO-ICT geschikt
         <v-icon>mdi-checkbox-marked-outline</v-icon> of ongeschikt
         <v-icon>mdi-checkbox-blank-outline</v-icon> tool
         </span>
     <v-spacer></v-spacer>
+    <v-btn class="ma-2" outlined color="alert" href="" target="_blank">
+          <v-icon>mdi-cached</v-icon>
+      <span>Herstart quiz</span>
+    </v-btn>
     <v-btn class="ma-2" outlined color="success" href="https://hz.nl/opleidingen/hbo-ict" target="_blank">
-      <span>Schrijf je in!</span>
+      <span>Schrijf je nu in!</span>
     </v-btn>
   </v-app-bar>
 </template>
