bdb5cc995ff523459fe136cd7afa54e0b0f6479a
Giorgio Joziasse <giorgio_giorgio@me.com>
2019-10-02 14:10:37 +0200

    added questioncomponent

--- src/components/QuizOnePage.vue
+++ src/components/QuizOnePage.vue
@@ -12,28 +12,7 @@
                   sm="8"
                   md="4"
           >
-              <v-card class="elevation-12">
-                  <v-toolbar
-                          color="primary"
-                          dark
-                          flat
-                  >
-                      <v-toolbar-title>Vraag 1</v-toolbar-title>
-                  </v-toolbar>
-                  <v-card-text>
-                      <v-form>
-                          <v-text-field
-                                  label="Wat is je pincode?"
-                                  name="login"
-                                  type="text"
-                          ></v-text-field>
-                      </v-form>
-                  </v-card-text>
-                  <v-card-actions>
-                      <div class="flex-grow-1"></div>
-                      <v-btn color="primary">Door naar vraag {{ nextQuestion }}</v-btn>
-                  </v-card-actions>
-              </v-card>
+
           </v-col>
       </v-row>
   </v-container>
@@ -42,56 +21,7 @@
 <script>
 export default {
   data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+    nextQuestion : "2"
+  })
 };
 </script>
