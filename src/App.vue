b11668ca26a6f54e864341f7aabba5fb1f8ad431
huib0029 <huib0029@hz.nl>
2019-10-30 19:52:06 +0100

    Final leaderboard page functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <Toolbar/>
+    <Toolbar />
     <v-content>
       <app-router></app-router>
     </v-content>
@@ -8,15 +8,21 @@
 </template>
 
 <script>
-import Toolbar from './layout/Toolbar';
-import AppRouter from './plugins/router';
+import Toolbar from "./layout/Toolbar";
+import AppRouter from "./plugins/router";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Toolbar,
     AppRouter
+  },
+  mounted() {
+    if (!localStorage.leaderboard) {
+      const a = [];
+      a.push(JSON.parse(localStorage.getItem("leaderboard")));
+      localStorage.setItem("leaderboard", JSON.stringify(a));
+    }
   }
 };
-
 </script>
