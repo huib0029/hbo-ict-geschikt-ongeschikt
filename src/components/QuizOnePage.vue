4544652a4edcf7b1e5647bd9d46ed431804a8077
huib0029 <huib0029@hz.nl>
2019-11-11 23:04:19 +0100

    Styling hotfix center quiz one container

--- src/components/QuizOnePage.vue
+++ src/components/QuizOnePage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="fill-height" fluid>
     <v-row align="center" justify="center">
-      <v-col cols="12" sm="8" md="4">
+      <v-col align="center" justify="center">
         <!--container-->
         <section class="container">
           <!--questionBox-->
